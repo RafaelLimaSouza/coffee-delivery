14e766e4e74b71a6c59bf6f1dae796ec18e34b1c
rafaellimasouza <rafael.lima1989@gmail.com>
2024-05-04 12:38:27 +0100

    feat: add immer to manipulate immutable items

--- src/reducers/cart/reducer.ts
+++ src/reducers/cart/reducer.ts
@@ -1,57 +1,52 @@
 import { CartActionType, CartActions } from './action'
 import { Order } from '../../contexts/cart.context'
+import { produce } from 'immer'
 
 export function cartReducer(state: Order[], action: CartActions) {
   switch (action.type) {
     case CartActionType.ADD_ITEM: {
-      const productIndex = state.findIndex(
-        (product) => product.productId === action.payload.productId,
-      )
+      return produce(state, (draft) => {
+        const foundProduct = draft.find(
+          (product: Order) => product.productId === action.payload.productId,
+        )
 
-      if (productIndex >= 0) {
-        state[productIndex] = {
-          productId: action.payload.productId,
-          quantity: action.payload.quantity,
+        if (foundProduct) {
+          foundProduct.quantity = action.payload.quantity
+        } else {
+          draft.push({
+            productId: action.payload.productId,
+            quantity: action.payload.quantity,
+          })
         }
-
-        return state
-      }
-
-      return [
-        ...state,
-        {
-          productId: action.payload.productId,
-          quantity: action.payload.quantity,
-        },
-      ]
+      })
     }
     case CartActionType.REMOVE_ITEM: {
-      return state.filter(
-        (product) => product.productId !== action.payload.productId,
+      return produce(state, (draft) =>
+        draft.filter(
+          (product) => product.productId !== action.payload.productId,
+        ),
       )
     }
     case CartActionType.ADD_UNIT: {
-      return state.map((product) => {
-        if (product.productId === action.payload.productId) {
-          return {
-            ...product,
-            quantity: product.quantity++,
+      return produce(state, (draft) => {
+        draft.map((product) => {
+          if (product.productId === action.payload.productId) {
+            product.quantity++
           }
-        }
 
-        return product
+          return product
+        })
       })
     }
     case CartActionType.REMOVE_UNIT: {
-      return state.map((product) => {
-        if (product.productId === action.payload.productId) {
-          return {
-            ...product,
-            quantity: product.quantity--,
+      return produce(state, (draft) => {
+        draft.map((product) => {
+          if (product.productId === action.payload.productId) {
+            product.quantity--
           }
-        }
 
-        return product
+          return product
+        })
       })
     }
     default:
